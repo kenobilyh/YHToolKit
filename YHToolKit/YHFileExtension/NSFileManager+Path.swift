752f9f83cc00429f789f8702b2043d4932e8cb99
Jeff Lin <yhlin@yahoo-inc.com>
2016-07-08 21:05:07 +0800

    upgrade to swift 3

--- YHToolKit/YHFileExtension/NSFileManager+Path.swift
+++ YHToolKit/YHFileExtension/NSFileManager+Path.swift
@@ -8,24 +8,24 @@
 
 import Foundation
 
-extension NSFileManager {
+extension FileManager {
     class func documentPath() -> String {
-        let docPathArray:[String] = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docPathArray:[String] = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         
-        return docPathArray.minElement()!
+        return docPathArray.min()!
     }
     
     class func cachePath() -> String{
-        let cachePathArray:[String] = NSSearchPathForDirectoriesInDomains(.CachesDirectory, .UserDomainMask, true)
+        let cachePathArray:[String] = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
         
-        return cachePathArray.minElement()!
+        return cachePathArray.min()!
     }
 }
 
-extension NSFileManager {
-    class func addSkipBackupAttributeToItemAtURL(url: NSURL) -> Bool {
+extension FileManager {
+    class func addSkipBackupAttributeToItemAtURL(_ url: URL) -> Bool {
         do {
-            try url.setResourceValue(NSNumber(bool: true), forKey: NSURLIsExecutableKey)
+            try (url as NSURL).setResourceValue(NSNumber(value: true), forKey: URLResourceKey.isExecutableKey)
             return true
         } catch {
             print("Error excluding %@ from backup", url.lastPathComponent)
@@ -34,14 +34,14 @@ extension NSFileManager {
     }
 }
 
-extension NSFileManager {
-    class func fileSizeForPath(filePath: String) -> CUnsignedLongLong {
+extension FileManager {
+    class func fileSizeForPath(_ filePath: String) -> CUnsignedLongLong {
         var fileSize: CUnsignedLongLong = 0;
-        let fileManager = NSFileManager()   // default is not thread safe
-        if (fileManager.fileExistsAtPath(filePath)) {
+        let fileManager = FileManager()   // default is not thread safe
+        if (fileManager.fileExists(atPath: filePath)) {
             var fileInfo: NSDictionary
             do {
-                fileInfo = try fileManager.attributesOfItemAtPath(filePath)
+                fileInfo = try fileManager.attributesOfItem(atPath: filePath)
                 fileSize = fileInfo.fileSize()
             } catch {
                 
